c00c034d6ea1c5e5cba34820a5559255714619b0
Mike the Tree Climber <mikethetreeclimber@gmail.com>
2022-08-29 18:55:55 -0400

    wip

--- app/Http/Livewire/CircuitCardSection.php
+++ app/Http/Livewire/CircuitCardSection.php
@@ -7,37 +7,34 @@ use Livewire\Component;
 
 class CircuitCardSection extends Component
 {
-    public $circuitNumber; 
-    public $totalWorksites;
-    public $totalPending;
-    public $totalAuthorized;
-    public $totalRefused;
+    public $circuitNumber;
     public $ghostUnits;
 
     public function mount()
     {
-        $this->totalWorksites = Worksite::where('circuit_number', $this->circuitNumber)->count();
-        $this->totalAuthorized = Worksite::where('circuit_number', $this->circuitNumber)
-        ->where('status', 'LIKE', '03 - Authorized')
-        ->count();
-
-        $this->totalPending = Worksite::where('circuit_number', $this->circuitNumber)
-        ->where('status', 'LIKE', '01 - Pending')
-        ->count();
-
-        $this->totalRefused = Worksite::where('circuit_number', $this->circuitNumber)
-        ->where('status', 'LIKE', '02 - Refused')
-        ->count();
+        
     }
 
     public function render()
     {
+        $totalWorksites = Worksite::where('circuit_number', $this->circuitNumber)->count();
+        $totalAuthorized = Worksite::where('circuit_number', $this->circuitNumber)
+        ->where('status', 'LIKE', '03 - Authorized')
+        ->count();
+
+        $totalPending = Worksite::where('circuit_number', $this->circuitNumber)
+        ->where('status', 'LIKE', '01 - Pending')
+        ->count();
+
+        $totalRefused = Worksite::where('circuit_number', $this->circuitNumber)
+        ->where('status', 'LIKE', '02 - Refused')
+        ->count();
+
         return view('livewire.circuit-card-section', 
-            compact(
-               [ $this->totalWorksites,
-                $this->totalAuthorized,
-                $this->totalPending,
-                $this->totalRefused,]
-            ));
+                compact('totalWorksites',
+                'totalAuthorized',
+                'totalPending',
+                'totalRefused',)
+            );
     }
 }
